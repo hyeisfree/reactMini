db8a51004ceac795057850e55e3ffc3a14f72e6f
hyeisfree <nahye6559@naver.com>
2024-03-29 17:50:31 +0900

    Counter 도전 중

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,19 @@
+import React from 'react'
+
 function Counter() {
   return (
     <>
-      <div></div>
+      <div>
+        <h2>Counter</h2>
+        <p>0</p>
+        <div className="btn_container">
+          <button className="inc">INCREASE</button>
+          <button className="res">RESET</button>
+          <button className="dec">DECREASE</button>
+        </div>
+      </div>
     </>
   )
 }
+
+export default Counter

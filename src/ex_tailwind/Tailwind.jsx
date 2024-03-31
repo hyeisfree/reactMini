bc345c09e592d5dc8385c61c7a8b24dfe598e6d7
hyeisfree <nahye6559@naver.com>
2024-03-31 22:32:19 +0900

    tailwind 연습용 폴더 및 파일 생성

---
+++ src/ex_tailwind/Tailwind.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tailwind() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Tailwind
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Tailwind from './ex_tailwind/Tailwind'
 // import App from "./App.jsx";
 // import "./index.css";
-import Counter from './Counter'
+// import Counter from './Counter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Counter />
+    {/* <Counter /> */}
+    <Tailwind />
   </React.StrictMode>,
 )

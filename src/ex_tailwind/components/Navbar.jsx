c0f66ed9fa802cba9f652d10b286bcc3753c13db
hyeisfree <nahye6559@naver.com>
2024-04-01 16:43:26 +0900

    tailwind 재설정 및 도전 중

--- src/ex_tailwind/components/Navbar.jsx
+++ src/ex_tailwind/components/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BsPerson } from 'react-icons/bs'
+import { BsSearch } from 'react-icons/bs'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center h-20">
+    <div className="flex justify-between items-center h-20 px-4">
       <div>
-        <h1>BEACHES</h1>
+        <h1>BEACHES.</h1>
+      </div>
+      <ul className="hidden md:block/flex/">
+        <li>Home</li>
+        <li>Destination</li>
+        <li>Travel</li>
+        <li>View</li>
+        <li>Book</li>
+      </ul>
+      <div className="hidden md:block/flex/">
+        <BsSearch />
+        <BsPerson />
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './ex_tailwind/components/Navbar'
-// import App from "./App.jsx";
-// import "./index.css";
+import './index.css'
+// import App from './App.jsx'
 // import Counter from './Counter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Counter /> */}
-    {/* <Tailwind /> */}
+    {/* <App /> */}
     <Navbar />
   </React.StrictMode>,
 )
